3506f521c5e84ef99253733a270cc4209255f886
Satish Wagle <satishwagle32.com>
2024-11-19 20:13:49 +0545

    feat: added all charts in dashboard

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = 'Zhouyi_CSCI6180'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:zhouyi1995@localhost/visualization1'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/visualization1'
